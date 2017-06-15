865156f46db10d653fcbec6c973af6fb107da511
frank.zheng <dingyuan.zheng1111@hotmail.com>
2017-06-15 16:11:10 +0800

    update seneca helloworld

--- seneca-demo/helloworld.js
+++ seneca-demo/helloworld.js
@@ -1,11 +1,37 @@
 'use strict';
 let seneca = require('seneca')()
 
+// 创建服务
+// 方式1
+//
 seneca.add('role:math,cmd:sum', (msg, reply) => {
   reply(null, {answer: (msg.left + msg.right)})
 })
 
-seneca.act({role: 'math', cmd: 'sum', left: 1, right: 2}, function (err, result) {
+// 方式2
+//
+let pattern = {
+  module: 'demo',
+  action: 'greeting'
+}
+
+let action = function(msg, done) {
+  done(null, { answer: `hello, ${msg.name}` })
+}
+seneca.add(pattern, action)
+
+
+// 客户端调用
+//
+// 方式1
+//
+seneca.act({role: 'math', cmd: 'sum', left: 1, right: 2}, (err, result) => {
   if (err) return console.error(err)
   console.log(result)
 })
+
+seneca.act({module:'demo', action: 'greeting', name: 'World'}, (err, result) => {
+  if (err) return console.error(err)
+  console.log(result)
+})
+
