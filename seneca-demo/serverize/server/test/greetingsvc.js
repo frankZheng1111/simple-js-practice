a1d70cf359a177a0963c7a296e995279bab8b7e0
frankZheng1111 <dingyuan.zheng1111@hotmail.com>
2017-06-18 20:05:52 +0800

    Add unit test

---
+++ seneca-demo/serverize/server/test/greetingsvc.js
@@ -0,0 +1,19 @@
+'use strict';
+let seneca = require('seneca')();
+let should = require ('chai').should();
+
+describe('when seneca use the greeting plugin', () => {
+  seneca.use('../greetingsvc.js');
+  it('should has plugin "greeting"', () => {
+    seneca.hasplugin('greeting').should.be.true;
+  });
+
+  it('should answer hellow with my name', function(){
+    let req = { module: 'demo', action: 'greeting', name:'Marlin' }
+    seneca.act( req, (error, result) => {
+      should.not.exist(error);
+      should.exist(result);
+      result.answer.should.eq('hello, '+req.name);
+    });
+  })
+})
